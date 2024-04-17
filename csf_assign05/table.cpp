fd318f117b9facdc14d28d4ad5db9ab88d4b3da7
LeahHersh <lhersh411@gmail.com>
2024-04-16 21:29:34 -0400

    Bug fix in table class

--- csf_assign05/table.cpp
+++ csf_assign05/table.cpp
@@ -34,21 +34,24 @@ void Table::set( const std::string &key, const std::string &value )
 std::string Table::get( const std::string &key )
 {
   // If the key is in the current table
-  if (this->key_value_pairs.find(key) != key_value_pairs.end()) {
+  if (this->key_value_pairs.find(key) != this->key_value_pairs.end()) {
 
-    return key_value_pairs.at(key);
+    return this->key_value_pairs.at(key);
   } 
   // If the key is in a proposed entry
-  else if (proposed_pairs.find(key) != proposed_pairs.end()) {
+  else if (this->proposed_pairs.find(key) != this->proposed_pairs.end()) {
 
-    return proposed_pairs.at(key);
+    return this->proposed_pairs.at(key);
   } 
 
 }
 
 bool Table::has_key( const std::string &key )
 {
-  if (key_value_pairs.find(key) != key_value_pairs.end() || proposed_pairs.find(key) != proposed_pairs.end()) {
+  // If the key is in the commited or proposed entry map
+  if (this->key_value_pairs.find(key) != this->key_value_pairs.end() || 
+      this->proposed_pairs.find(key) != this->proposed_pairs.end()) {
+
     return true;
   }
 
@@ -58,12 +61,12 @@ bool Table::has_key( const std::string &key )
 void Table::commit_changes()
 {
   // Add every entry in the map with new or edited table entries to the commited table
-  for (auto it : proposed_pairs) {
-    key_value_pairs[it.first] = it.second;
+  for (auto it : this->proposed_pairs) {
+    this->key_value_pairs[it.first] = it.second;
   }
 }
 
 void Table::rollback_changes()
 {
-  key_value_pairs.clear();
+  this->proposed_pairs.clear();
 }
