cf4391caf2440a0acce049ac135e64a0410745fd
LeahHersh <lhersh411@gmail.com>
2024-04-20 14:19:27 -0400

    MessageSerialization refactoring

--- csf_assign05/message_serialization.cpp
+++ csf_assign05/message_serialization.cpp
@@ -5,12 +5,7 @@
 #include "exceptions.h"
 #include "message_serialization.h"
 
-void check_message_size(std::string msg) {
 
-  if (msg.length() > Message::MAX_ENCODED_LEN) {
-    throw InvalidMessage("Message is too long.");
-  }
-}
 
 void MessageSerialization::encode( const Message &msg, std::string &encoded_msg )
 {
@@ -55,6 +50,7 @@ void MessageSerialization::encode( const Message &msg, std::string &encoded_msg 
     break;
   case MessageType::DATA: encoded_msg = "DATA";
     break;
+  default: throw InvalidMessage("Message must have a type.");
   }
 
   // Add message arguments to the encoded message
@@ -134,6 +130,8 @@ void MessageSerialization::decode( const std::string &encoded_msg_, Message &msg
   }
   else if (m_type == "DATA") {
     msg.set_message_type(MessageType::DATA);
+  } else {
+    throw InvalidMessage("Message must have a type.");
   }
 
   // Clear msg's arguments
@@ -141,37 +139,56 @@ void MessageSerialization::decode( const std::string &encoded_msg_, Message &msg
 
   // If the rest of the string is a quoted text, push it as the only argument
   if (msg.get_message_type() == MessageType::FAILED || msg.get_message_type() == MessageType::ERROR) {
-
     std::string quoted_text = ss.str();
-
-    if (quoted_text.back() != '\n') {
-      throw InvalidMessage("Message is missing a newline.");
-    }
-
-    int open_quote_ind = quoted_text.find_first_of('"');
-    int close_quote_ind = quoted_text.find_last_of('"');
-
-    quoted_text = quoted_text.substr(open_quote_ind + 1, close_quote_ind - open_quote_ind - 1);
-    msg.push_arg(quoted_text);
-
-    if (!msg.is_valid()) {
-      throw InvalidMessage("Message is not valid.");
-    }
-    return;
+    extract_quoted_text_arg(msg, quoted_text);
+  }
+  // Otherwise, set the decoded message's arguments word by word
+  else {
+    extract_single_word_args(msg, ss);
   }
 
-  // Set the decoded message's arguments
-  std::string curr_arg;
-  while(ss >> curr_arg) {
-    msg.push_arg(curr_arg);
-  }
-
+  // If the resulting Message is not valid
   if (!msg.is_valid()) {
     throw InvalidMessage("Message is not valid.");
   }
 
+  // If the encoded message did not end with a newline
   std::string left_over = ss.str();
   if (left_over.back() != '\n') {
-    throw InvalidMessage("Message is missing a newline.");
+    throw InvalidMessage("Encoded message is missing a newline.");
   }
 }
+
+
+void MessageSerialization::check_message_size(std::string msg) {
+
+  if (msg.length() > Message::MAX_ENCODED_LEN) {
+    throw InvalidMessage("Message is too long.");
+  }
+}
+
+
+void MessageSerialization::extract_quoted_text_arg(Message msg, std::string quoted_text) {
+
+    size_t open_quote_ind = quoted_text.find_first_of('"');
+    size_t close_quote_ind = quoted_text.find_last_of('"');
+
+    // If none of the text is surrounded by quotations
+    if (open_quote_ind == std::string::npos || close_quote_ind == std::string::npos) {
+      throw InvalidMessage("Message whose type requires a quoted text argument does not contain a quotation.");
+    }
+
+    quoted_text = quoted_text.substr(open_quote_ind + 1, close_quote_ind - open_quote_ind - 1);
+    msg.push_arg(quoted_text);
+}
+
+
+void MessageSerialization::extract_single_word_args(Message msg, std::stringstream& ss) {
+  
+  std::string curr_arg;
+  while(ss >> curr_arg) {
+    msg.push_arg(curr_arg);
+  }
+}
+
+
--- csf_assign05/message_serialization.h
+++ csf_assign05/message_serialization.h
@@ -4,8 +4,33 @@
 #include "message.h"
 
 namespace MessageSerialization {
+
   void encode(const Message &msg, std::string &encoded_msg);
+
   void decode(const std::string &encoded_msg, Message &msg);
+
+  /* 
+   * Throws an InvalidMessage exception if an encoded Message exceeds MAX_ENCODED_LEN. 
+   * @param msg The encoded message.
+   * @throws InvalidMessage exception.
+  */
+  void check_message_size(std::string msg);
+
+  /* 
+  * Finds the text between quotation marks and pushes it to a Message's arguments.
+  * @param msg The encoded message.
+  * @param quoted_text The text between the quotation marks.
+  * @throws InvalidMessage if the encoded Message doesn't include a quotation.
+  */
+  void extract_quoted_text_arg(Message msg, std::string quoted_text);
+
+  /* 
+  * Takes a stringstream of arguments and pushes each word in the stringstream as a Message's argument.
+  * @param msg The encoded message.
+  * @param ss The stringstream of arguments.
+  */
+  void extract_single_word_args(Message msg, std::stringstream& ss);
+
 };
 
 #endif // MESSAGE_SERIALIZATION_H
